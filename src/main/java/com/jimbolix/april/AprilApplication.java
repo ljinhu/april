dfe6263f663a16b2ab35ab864cd74fc27a945f0d
ljinhu <jimbolix@163.com>
2020-04-01 11:35:07 +0800

    初始化项目

---
+++ src/main/java/com/jimbolix/april/AprilApplication.java
@@ -0,0 +1,13 @@
+package com.jimbolix.april;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jimbolix/april/AprilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimbolix.april;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
