0c8008573f5cc87a3705394f9ef49319cc5e7651
ljinhu <jimbolix@163.com>
2020-04-14 22:32:07 +0800

    SimpleMessageListenerContainer 简单消息监听容器的简单配置

--- src/main/java/com/jimbolix/april/rabbitmq/config/AprilRabbitConfig.java
+++ src/main/java/com/jimbolix/april/rabbitmq/config/AprilRabbitConfig.java
@@ -1,13 +1,20 @@
 package com.jimbolix.april.rabbitmq.config;
 
 import com.jimbolix.april.rabbitmq.peoperties.AprilRabbitMqConfigProperties;
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
+import org.springframework.amqp.support.ConsumerTagStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.UUID;
 
 @Configuration
 public class AprilRabbitConfig {
@@ -36,5 +43,27 @@ public class AprilRabbitConfig {
     public RabbitTemplate rabbitTemplate(CachingConnectionFactory connectionFactory){
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         return rabbitTemplate;
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer simpleMessageListenerContainer(ConnectionFactory connectionFactory){
+        SimpleMessageListenerContainer smListenerContainer = new SimpleMessageListenerContainer(connectionFactory);
+        smListenerContainer.addQueueNames("");//绑定哪些队列
+        smListenerContainer.setMaxConcurrentConsumers(5);//最多多少消费者
+        smListenerContainer.setConcurrentConsumers(1);//
+        smListenerContainer.setConsumerTagStrategy(new ConsumerTagStrategy() {
+            @Override
+            public String createConsumerTag(String queue) {
+                return queue +"_"+ UUID.randomUUID().toString();
+            }
+        });
+
+        smListenerContainer.setMessageListener(new ChannelAwareMessageListener() {
+            @Override
+            public void onMessage(Message message, Channel channel) throws Exception {
+                //消息监听处理
+            }
+        });
+        return smListenerContainer;
     }
 }
